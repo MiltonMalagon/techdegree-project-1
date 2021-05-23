40211aa2d03c514a975d90f6c160dc08134a7b31
MiltonMalagon <miltonmalagon1985@gmail.com>
2021-05-23 09:21:12 -0500

    Added random quote variable
    Added random quote variable to function getRandomQuote and changed Math.ceil() method by Math.floor().

--- js/script.js
+++ js/script.js
@@ -11,68 +11,73 @@ project 1 - A Random Quote Generator
  * `quotes` array 
  ***/
 const quotes = [{
+        citation: "Client-Side Programming with JavaScript",
         quote: "If you thought building web pages was cool, you're going to love what you can do with a little programming.",
         source: "― Andy Harris",
-        citation: "Client-Side Programming with JavaScript",
         year: 2014
     },
     {
+        citation: "Interview with the Vampire",
         quote: "The only power that exists is inside ourselves.",
         source: "― Anne Rice",
-        citation: "Interview with the Vampire",
         year: 1976
     },
     {
+        citation: "The Happy Prince and Other Stories",
         quote: "I am so clever that sometimes I don't understand a single word of what I am saying.",
         source: "― Oscar Wilde",
-        citation: "The Happy Prince and Other Stories",
         year: 1888
     },
     {
+        citation: "Harry Potter and the Chamber of Secrets",
         quote: "It is our choices, Harry, that show what we truly are, far more than our abilities.",
         source: "― J.K. Rowling",
-        citation: "Harry Potter and the Chamber of Secrets",
         year: 1998
     },
     {
+        citation: "The Alchemist",
         quote: "When we love, we always strive to become better than we are. When we strive to become better than we are, everything around us becomes better too.",
         source: "― Paulo Coelho",
-        citation: "The Alchemist",
         year: 1988
     },
     {
+        citation: "The Fellowship of the Ring",
         quote: "All we have to decide is what to do with the time that is given us.",
         source: "― J.R.R. Tolkien",
-        citation: "The Fellowship of the Ring",
         year: 1954
     },
     {
+        citation: "Les Misérables",
         quote: "He never went out without a book under his arm, and he often came back with two.",
         source: "― Victor Hugo",
-        citation: "Les Misérables",
         year: 1862
     },
     {
+        citation: "Dracula",
         quote: "I am longing to be with you, and by the sea, where we can talk together freely and build our castles in the air.",
         source: "― Bram Stoker",
-        citation: "Dracula",
         year: 1897
     },
     {
+        citation: "Alice in Wonderland",
         quote: "Who in the world am I? Ah, that's the great puzzle.",
         source: "― Lewis Carroll",
-        citation: "Alice in Wonderland",
         year: 1865
     }
 ];
+
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
  ***/
 function getRandomQuote(arr) {
-    let randomNumber = Math.ceil(Math.random() * arr.length);
-    return randomNumber;
+    let randomNumber = Math.floor(Math.random() * arr.length);
+    let randomObject = arr[randomNumber];
+    console.log(randomNumber, randomObject);
 }
+
+getRandomQuote(quotes);
 
 /***
  * `printQuote` function
